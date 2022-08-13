1b79c863ada3d3c52c8ba4d8c57d6e38587360cb
abista010 <56502203+abista010@users.noreply.github.com>
2022-08-13 17:14:26 -0400

    added index.html and style.css

--- secondAuth.py
+++ secondAuth.py
@@ -3,4 +3,6 @@ import json
 Headers = {'x-hydra-api-key':'51586fdcbd214feb84b0e475b130fce0','x-hydra-user-agent':'Hydra-Cpp/1.132.0','Content-Type':'application/json','x-hydra-client-id':'47201f31-a35f-498a-ae5b-e9915ecb411e'}
 Body = { "auth": { "fail_on_missing": 1, "steam": "080110b4bdc0cf071800203a2a70c6f422ada50b0c9b35cc973c9d3a26e1986282d3c8ba04659bf6e569d9a7fc5e557ce6baa0ada0111596686d0a8e66d8f5affa7a310da4cc417f8de729eaedb7af4180977f005d27f143b1d6fd31bb7facae7e9b5316450ba55ca45fdc1351c47ab833e0ebe865e7a52432b81da20d68" }, "options": [ "wb_network" ] }
 req = requests.post("https://dokken-api.wbagora.com/access", json=Body, headers=Headers).json()
-print(req)
+print(req['token'])
+print(req['wb_network']['network_token'])
+
--- thirdAuth.py
+++ thirdAuth.py
@@ -1,6 +1,6 @@
 import requests
 import json
-Headers = {'x-hydra-api-key':'15189024601c48fa94e7dfe62a6a3cb6','x-hydra-user-agent':'Hydra-Cpp/1.132.0','Content-Type':'application/json'}
-Body = { "code": "I+MPokHx/JkCFmuHpcEiaqyzIV/BJGsRHnFwTEiKxk6TdlwIaolu4wyL6h5q3C62Ljs3DvzZRN8+oFHMwDtsifmzQftbOxFJUVS9c9ijMO0=","grant_type":"authorization_code" }
-req = requests.post("https://prod-network-api.wbagora.com/sessions/auth/token", json=Body, headers=Headers).json()
-print(req)
+Headers2 = {'x-hydra-api-key':'15189024601c48fa94e7dfe62a6a3cb6','x-hydra-user-agent':'Hydra-Cpp/1.132.0','Content-Type':'application/json'}
+Body2 = { "code": "I+MPokHx/JkCFmuHpcEiaqyzIV/BJGsRHnFwTEiKxk6TdlwIaolu4wyL6h5q3C62Ljs3DvzZRN8+oFHMwDtsifmzQftbOxFJUVS9c9ijMO0=","grant_type":"authorization_code" }
+req2 = requests.post("https://prod-network-api.wbagora.com/sessions/auth/token", json=Body2, headers=Headers2).json()
+print(req2)
